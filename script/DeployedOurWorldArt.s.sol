f07066c1eecd9a1ca7209c8787203dc9a05fed6f
MarcBTHT <marc.berthol29@gmail.com>
2024-02-13 19:23:09 +0100

    feat: :construction: OurWorldArt smart contract build
    safeMint, updateMatrixCell, resetMatrix. Need to do test for this!

---
+++ script/DeployedOurWorldArt.s.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Script, console2} from "forge-std/Script.sol";
+import {OurWorldArt} from "../src/OurWorldArt.sol";
+
+contract DeployedOurWorldArt is Script {
+    address public myTestContractAddress = 0x7FA9385bE102ac3EAc297483Dd6233D62b3e1496; //Address of test contract
+    address public myAnvil0Address = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266; //Anvil 0
+    address public mySepoliaAddress = 0xc29dEE36CEaF01B276002CDF894814A2b3Fa8aAf;
+
+    function run() external returns (OurWorldArt) {
+        vm.startBroadcast();
+        OurWorldArt ourWorldArt = new OurWorldArt(mySepoliaAddress); 
+        vm.stopBroadcast();
+        return ourWorldArt;
+    }
+}
---
+++ src/OurWorldArt.sol
@@ -0,0 +1,137 @@
+// Layout of Contract:
+    // version
+    // imports
+    // errors
+    // interfaces, libraries, contracts
+    // Type declarations
+    // State variables
+    // Events
+    // Modifiers
+    // Functions
+
+// Layout of Functions:
+    // constructor
+    // receive function (if exists)
+    // fallback function (if exists)
+    // external
+    // public
+    // internal
+    // private
+    // view & pure functions
+
+// SPDX-Licence-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+import "lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+import "lib/openzeppelin-contracts/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "lib/openzeppelin-contracts/contracts/access/Ownable.sol";
+import "lib/openzeppelin-contracts/contracts/utils/Base64.sol";
+import "lib/openzeppelin-contracts/contracts/utils/Strings.sol";
+
+// import {console} from "forge-std/Script.sol"; 
+
+contract OurWorldArt is ERC721, ERC721URIStorage, Ownable {
+
+    // ================================================================
+    // |                          Errors                              |
+    // ================================================================
+
+    // ================================================================
+    // |                   Type declarations                          |
+    // ================================================================
+
+    // ================================================================
+    // |                    STATE VARIABLES                           |
+    // ================================================================
+
+    //Contract variables
+    uint256 private _nextTokenId;
+    uint public constant MATRIX_SIZE = 50;
+    uint24[MATRIX_SIZE][MATRIX_SIZE] public matrix; // Use uint24 to store HEX color values (Put it public or private ?)
+    bool public matrixFinalized = false; // Status to check for matrix updates
+
+    // ================================================================
+    // |                          Events                              |
+    // ================================================================
+
+    // ================================================================
+    // |                          Modifiers                           |
+    // ================================================================
+
+    // ================================================================
+    // |                          Functions                           |
+    // ================================================================
+
+    constructor(address initialOwner)
+        ERC721("OurWorldArt", "OWA")
+        Ownable(initialOwner)
+    {}
+
+    function safeMint(address to, string memory uri) public onlyOwner {
+        require(!matrixFinalized, "Matrix already minted.");
+        matrixFinalized = true; // Prevents any future modification of the matrix
+        uint256 tokenId = _nextTokenId++;
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri); 
+    }
+
+    // Function to update a matrix cell with a color value
+    function updateMatrixCell(uint x, uint y, uint24 hexValue) public {
+        require(!matrixFinalized, "Matrix updates are locked."); // Checks if the matrix is locked
+        require(x < MATRIX_SIZE && y < MATRIX_SIZE, "Out of bounds");
+        matrix[x][y] = hexValue;
+    }
+
+    function resetMatrix() private {
+        for(uint i = 0; i < MATRIX_SIZE; i++) {
+            for(uint j = 0; j < MATRIX_SIZE; j++) {
+                matrix[i][j] = 0; // Resets each cell to the default value
+            }
+        }
+        matrixFinalized = false; // Unlocks the matrix for the next cycle
+    }
+
+    // ================================================================
+    // |                          METADATA                            |
+    // ================================================================
+    function _baseURI() internal pure override returns (string memory) {
+        return "data:application/json;base64,";
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        string memory imageURI = ""; // TO DO
+
+        return 
+           string(
+            abi.encodePacked(
+                    _baseURI(),
+                    Base64.encode(
+                        bytes(
+                            abi.encodePacked(
+                                '{"PixelMatrix":"',
+                                matrix, 
+                                '", "image":"',
+                                imageURI,
+                                '"}'
+                            )
+                        )
+                    )
+                )
+        );
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
